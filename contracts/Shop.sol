260375c272c5eddf0b44d0c01f7a688d05aabbce
Andrii <handyhd02@gmail.com>
2021-12-19 13:02:04 +0200

    shop

--- contracts/Shop.sol
+++ contracts/Shop.sol
@@ -12,6 +12,8 @@ import "./../contracts-upgradeable/metatx/ERC2771ContextUpgradeable.sol";
 contract Shop is Initializable, AccessControlUpgradeable, PausableUpgradeable, ERC2771ContextUpgradeable {
 
 
+
+    bytes32 public constant USER_ROLE = keccak256("USER_ROLE");
     function _msgSender() internal override(ContextUpgradeable, ERC2771ContextUpgradeable) view returns (address) {
         return ERC2771ContextUpgradeable._msgSender();
     }
@@ -19,5 +21,27 @@ contract Shop is Initializable, AccessControlUpgradeable, PausableUpgradeable, E
         return ERC2771ContextUpgradeable._msgData();
     }
 
+    function initialize(address _admin)public initializer{
+        __Context_init_unchained();
+        __AccessControl_init();
+        __Pausable_init_unchained();
+        
+        _setupRole(DEFAULT_ADMIN_ROLE, _admin);
+
+
+    }
+
+    modifier onlyAdmin() {
+        require(hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), "Caller is not the Admin");
+        _;
+    }
+
+    function pause() external onlyAdmin {
+        _pause();
+    }
+
+    function unpause() external onlyAdmin {
+        _unpause();
+    }
 
 }
